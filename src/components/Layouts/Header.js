5c9da27e67078ae5444c3508e3e5d40655c0dbce
Petr Rikhter <petr_rikhter@mail.ru>
2022-11-21 21:24:30 +0600

    added header

---
+++ src/components/Layouts/Header.js
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css";
+import React from "react";
+import sushiImage from "../../assets/sushi.jpg";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1>Sushi shop</h1>
+        <button>Корзина</button>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={sushiImage} alt="Sushi" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
