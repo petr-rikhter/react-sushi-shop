1ac1f72f604cacd1493af001d34906dbbef6ac5b
Petr Rikhter <petr_rikhter@mail.ru>
2022-11-22 21:09:47 +0600

    added portal for modal window

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layouts/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />;
       <main>
         <Meals />
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Modal from "../UI/Modal";
+import styles from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "m2", key: "1", name: "Sushi", amount: 2, price: 10.99 }].map(
+        (item) => (
+          <li>{item.name}</li>
+        )
+      )}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Итого</span>
+        <span>49.99</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]}>Закрыть</button>
+        <button className={styles.button}>Заказать</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
