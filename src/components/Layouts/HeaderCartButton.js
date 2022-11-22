c55d94b6bb761c5741e819e24c69658ffa8eacd4
Petr Rikhter <petr_rikhter@mail.ru>
2022-11-22 23:46:12 +0600

    added useReducer

--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -1,15 +1,24 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const cartItemsNumbers = cartContext.items.reduce(
+    (current, item) => current + item.amount,
+    0
+  );
+
   return (
     <button onClick={props.onShow} className={styles.button}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Корзина</span>
-      <span className={styles.badge}>2</span>
+      <span className={styles.badge}>{cartItemsNumbers}</span>
     </button>
   );
 };
