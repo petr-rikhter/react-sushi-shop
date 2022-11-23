92d35c67c3469b7d85dfd0647bffbd0c7af9eef4
Petr Rikhter <petr_rikhter@mail.ru>
2022-11-24 01:48:24 +0600

    added a functionality delete meals in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,11 +8,16 @@ import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartContext = useContext(CartContext);
 
-  const totalAmount = `${cartContext.totalAmount.toFixed(2)} $`;
+  const totalAmount = `${Math.abs(cartContext.totalAmount).toFixed(2)} $`;
   const hasItems = cartContext.items.length > 0;
 
-  const removeCartItemHandler = (id) => {};
-  const addCartItemHandler = (item) => {};
+  const removeCartItemHandler = (id) => {
+    cartContext.removeItem(id);
+  };
+
+  const addCartItemHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -42,6 +42,33 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex((item) => {
+      return item.id === action.id;
+    });
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
@@ -60,7 +87,7 @@ const CartContextProvider = (props) => {
 
   const removeItemHandler = (id) => {
     dispatchCartAction({
-      type: "REMOVE",
+      type: "REMOVE_ITEM",
       id: id,
     });
   };
